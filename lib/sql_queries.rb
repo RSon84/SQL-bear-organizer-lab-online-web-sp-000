b4c2694fec8fb26c5d8b4084d64cbb224f6853b0
Devin Wilson <craigvin84@yahoo.com>
2021-04-14 15:04:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'SHY';"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'Shy';"
 end
 
 def selects_bear_that_killed_Tim
